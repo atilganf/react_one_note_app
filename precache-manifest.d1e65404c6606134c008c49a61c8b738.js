45172732c1838b0aa1ddf3f97c930cf56abad89c
atilganf <atilganyf@gmail.com>
2019-09-22 16:15:02 +0300

    Updates

---
+++ precache-manifest.d1e65404c6606134c008c49a61c8b738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8c9e47a71447b62f0e9b99294f00a0",
+    "url": "/react_one_note_app/index.html"
+  },
+  {
+    "revision": "eae1ec22a6002706420c",
+    "url": "/react_one_note_app/static/css/main.880cc460.chunk.css"
+  },
+  {
+    "revision": "30c786af52f7d0c4aa7f",
+    "url": "/react_one_note_app/static/js/2.d5c4a133.chunk.js"
+  },
+  {
+    "revision": "eae1ec22a6002706420c",
+    "url": "/react_one_note_app/static/js/main.ce2aaf65.chunk.js"
+  },
+  {
+    "revision": "2a8879d776a5ed7cdec3",
+    "url": "/react_one_note_app/static/js/runtime~main.f334ee9b.js"
+  }
+]);
